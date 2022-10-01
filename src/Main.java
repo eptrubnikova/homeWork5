50056af40be4ba6f1c54457255fc441ada992c65
eptrubnikova <eptrubnikova@gmail.com>
2022-10-02 00:37:27 +0600

    домашняя работа 5

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,34 @@ public class Main {
             else
                 System.out.println("Установите приложение для Android по ссылке");}
         else System.out.println("У вас неизвестная операционная система");
+
+        System.out.println("Задача 4.5");
+        int monthNumber = 5;
+
+        switch (monthNumber ) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Сейчас зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Сейчас весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Сейчас лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Сейчас осень");
+                break;
+            default:
+                System.out.println("такого месяца не существует");
+        }
         }
 
     }
